1fd02eef8b77feca0aa5966210ac9ae0e911207f
Abdusamadov-Sardor <sardorabdusamatov@750gmail.com>
2022-04-11 14:06:54 +0500

    Initial Commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+alert("Assalomu alaykum");
+
+var enterAge =parseInt(prompt("Yoshingizni kiriting?"),10);
+
+  
+  if( enterAge >= 16){
+    var go = "Poygada qatnashishingiz mumkin"
+    console.log(go);
+    var title = document.createElement("h1")
+    go.textContent = "enterAge"
+    document.body.append(go)
+  
+  }else {
+       var stop = "Poygada qatnasha olmaysiz uzr"
+       console.log(stop);
+  var title = document.createElement("h1")
+  stop.textContent = "enterAge"
+  document.body.append(stop)
+}
+ 
